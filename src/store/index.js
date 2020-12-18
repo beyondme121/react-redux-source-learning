664588c4ea37fd198011b53d46baf6419ff7143a
ASIAPACIFIC\CNZHLIU14 <beyondme121@163.com>
2020-12-18 17:08:15 +0800

    redux-middleware-1

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,19 @@
 import { createStore } from '../redux'
 import reducers from './reducers'
 
-let store = createStore(reducers)
-window.store = store
+// let store = createStore(reducers)
+// window.store = store
+// export default store
+const store = createStore(reducers, {
+  counter1: { number: 10 },
+  counter2: { number: 10000 },
+})
+
+let dispatch = store.dispatch
+store.dispatch = function (action) {
+  console.log(store.getState(), '-------------> ???')
+  dispatch(action)
+  console.log(store.getState(), '<------------- !!!')
+  return action
+}
 export default store
